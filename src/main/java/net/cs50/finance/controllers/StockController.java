42ff501a109797a961a5ef2b2a28044e3ea61521
Chris Bay <christopherbay@gmail.com>
2015-05-18 08:25:39 -0500

    Implement quote

--- src/main/java/net/cs50/finance/controllers/StockController.java
+++ src/main/java/net/cs50/finance/controllers/StockController.java
@@ -1,6 +1,11 @@
 package net.cs50.finance.controllers;
 
+import net.cs50.finance.models.Stock;
+import net.cs50.finance.models.StockLookupException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Chris Bay on 5/17/15.
@@ -9,5 +14,28 @@ import org.springframework.stereotype.Controller;
 public class StockController extends AbstractFinanceController {
 
     // TODO - implement quote, buy, sell
+
+    @RequestMapping(value = "/quote", method = RequestMethod.GET)
+    public String quoteForm() {
+        return "quote_form";
+    }
+
+    @RequestMapping(value = "/quote", method = RequestMethod.POST)
+    public String quote(String symbol, Model model) {
+
+        Stock stock;
+
+        try {
+            stock = Stock.lookupStock(symbol);
+        } catch (StockLookupException e) {
+            e.printStackTrace();
+            return displayError("Error retrieving info for stock: " + symbol, model);
+        }
+
+        model.addAttribute("stock_desc", stock.getSymbol() + " (" + stock.getName() + ")");
+        model.addAttribute("stock_price", "" + stock.getPrice());
+
+        return "quote_display";
+    }
 
 }
